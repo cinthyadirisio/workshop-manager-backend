8efba5bda649a33bbdaa39a72cfc0c53f6e8e3bc
Cinthya Di Risio <cindy.fdb@hotmail.com>
2024-09-24 15:15:11 -0300

    separados los endpoint de user y authorization

---
+++ src/routes/userRouter.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import userController from '../controllers/authController.js'
+import passport from '../middlewares/passport/passport.js'
+
+const userRouter = express.Router()
+
+userRouter.get( '/', userController.getAllUsers )
+userRouter.get( '/:id', userController.getOneUserByID )
+userRouter.delete( '/:id', userController.deleteUser )
+userRouter.put( '/:id' , passport.authenticate('jwt', { session: false }), userController.updateUser )
+
+export default userRouter
