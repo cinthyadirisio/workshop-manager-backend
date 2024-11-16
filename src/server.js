dace54a319f07095b06d242a4f282563ead43802
Cinthya Di Risio <cindy.fdb@hotmail.com>
2024-11-16 14:04:23 -0300

    fix?

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import express from 'express' 
+import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import indexRouter from './routes/indexRouter.js'
@@ -7,11 +7,11 @@ import connectDB from './config/db.js'
 import errorHandler from './utils/errorHandler.js'
 dotenv.config()
 const server = express()
-server.use( express.json() )
-server.use( cors() )
-server.use( logger )
+server.use(express.json())
+server.use(cors())
+server.use(logger)
 connectDB()
-server.use( '/api', indexRouter )
-server.get( '/', (request, response) => { response.send('welcome') } )
-server.use( errorHandler )
-server.listen( process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`) )
+server.use('/api', indexRouter)
+server.get('/', (request, response) => { response.send('welcome') })
+server.use(errorHandler)
+server.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
